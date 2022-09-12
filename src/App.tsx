eea59a312bd00dcb668950a29376d4e790e8031a
Markus-js <61780685+Markus-js@users.noreply.github.com>
2022-09-12 12:14:57 +0200

    scss
    Co-authored-by: Molly Sun <Molls13@users.noreply.github.com>
    Co-authored-by: Maximilian Skjønhaug <MaximilianLS98@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
+import './App.scss';
+
+interface Location {
+  city: string,
+  country: string,
+  postcode: number,
+  state: string,
+  street?: any
+}
+
+interface Name {
+  first: string,
+  last: string,
+  title: string
+}
+// interface IUser {
+//   name: Name,
+//   address: Location,
+//   age: number
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+  const [name, setName] = useState<Name | null>(null);
+  const [age, setAge] = useState<number | null>(null);
+  const [location, setLocation] = useState<Location | null>(null);
+
+  const inputField = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/')
+      .then(res => res.json())
+      .then(data => {
+        setName(data.results[0].name)
+        setAge(data.results[0].dob.age);
+        setLocation(data.results[0].location);
+      })
+  }, [])
+
+  const changeName = () => {
+    if (inputField.current && name) {
+      setName({ ...name, first: inputField.current.value })
+      inputField.current.value = '';
+    }
+  }
+
+  return name ? (
+    <>
+      <section className="input-container">
+        <input ref={inputField} type="text" />
+        <button onClick={changeName}>Change Name</button>
+      </section>
+      <section className="user-container">
+        <h2>{name.title} {name.first} {name.last}</h2>
+      </section>
+      <section className="location-container">
+        <h3>Location</h3>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {location?.street.name} {location?.street.number}, {location?.postcode} {location?.city} {location?.country}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h3>Age</h3>
+        <p>{age}</p>
+      </section>
+    </>
+  ) : null;
 }
 
 export default App;
